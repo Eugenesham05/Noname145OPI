4a4b1f29d852c4f7ac263a6f2856483c6377c580
Eugene <olia.shamray82@gmail.com>
2023-11-21 21:08:46 +0200

    int

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Enter x =";
+	cin >> x;
+	float rez;
+	int sum = 0;
+	for (int n = 1; n < 4; n++)
+	{
+		sum =cos(sum)+n * x;
+	}
+
+	rez = log(x) + sum;
+	cout << "Y=" << rez << endl;
+
+	system("pause");
+	return 0;
+}
+
+
